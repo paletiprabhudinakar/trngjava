7b9d1d43529f3e300bc43067aa6510bd3f363812
paletiprabhudinakar <129029605+paletiprabhudinakar@users.noreply.github.com>
2023-03-27 10:31:47 +0530

    Add files via upload

---
+++ home.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Prog
+{
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n1=sc.nextInt();
+int n2=sc.nextInt();
+int res=n1/n2;
+System.out.println(n1+"/"+n2+"="+res);
+}
+}
